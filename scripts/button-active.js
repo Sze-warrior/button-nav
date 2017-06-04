28492c5f3f7e2d6e5ebfbf9212b066344d82a8f6
Michael Szeto <szetomj@gmail.com>
2017-06-05 00:53:42 +1200

    added new velocity effects to buttons

--- scripts/button-active.js
+++ scripts/button-active.js
@@ -2,11 +2,27 @@ $(document).ready(function(){
 
 	var buttonContainer = $(".btn-nav-container");
 	var buttonContainerChildren = buttonContainer.children();
+   	var target = $(this).attr('href');
 
 	function addActive(){
 		buttonContainerChildren.removeClass("active");
 		$(this).addClass("active");
 	};
 
+	function FlipXIn(event){
+		event.preventDefault();
+		event.stopPropagation();
+
+       var target = $(this).attr('href');
+        $(target).velocity('transition.slideDownIn', {
+            duration: 500
+        });
+        $(target).velocity('transition.slideUpOut', {
+            duration: 500, delay: 1000
+        });
+
+	}
+
 	buttonContainerChildren.on("click", addActive);
+	$("a.btn-nav").on("click", FlipXIn);
 });
