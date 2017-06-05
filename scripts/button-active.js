313885ee7e464faad7075d0b844e5b1042f8b366
Michael Szeto <szetomj@gmail.com>
2017-06-05 15:19:17 +1200

    Reducing jumping when toggling content for better UX visually
    Reducing jumping when toggling content for better UX visually

--- scripts/button-active.js
+++ scripts/button-active.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
 
 	var buttonContainer = $(".btn-nav-container");
 	var buttonContainerChildren = buttonContainer.children();
-   	var target = $(this).attr('href');
-
+   	var btnNavContent = $(".btn-nav-content");
 
 	function addActive(e){
 		buttonContainerChildren.removeClass("btn-active");
@@ -11,17 +10,15 @@ $(document).ready(function(){
 	};
 
 	function FlipXIn(event){
+       	$(btnNavContent).children().css("display", "none");//Need to edit with inline CSS to override velocity
+
 		event.preventDefault();
 		event.stopPropagation();
-
-       	var target = $(this).attr('href');
-
-       	$(".content").children().css("display", "none");
+	   	var target = $(this).attr('href');//This var needs to stay local to avoid effecting all a tags.
 
        	$(target).addClass("visible");
-        $(target).velocity('transition.fadeIn', {
-            duration: 200
-        });
+        $(target).velocity({translateY:"-60px"}, { duration: 260});
+        $(target).velocity('transition.slideDownIn', { duration: 200});
 	};
 
 	buttonContainerChildren.on("click", addActive);
