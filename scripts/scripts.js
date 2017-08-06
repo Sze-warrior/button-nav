b0bee11002bd92b51790401477dd2e4fc1db0821
Michael Szeto <szetomj@gmail.com>
2017-08-06 21:06:20 +1200

    fixed interaction to work only on targeted element

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,21 @@
 var btnNavSwitch = document.querySelectorAll('.btn-nav');
+var btnNavContainer = document.querySelectorAll('.btn-nav-container');
 
-if(btnNavSwitch){
-	function setActive(e){
-		var target = e.currentTarget;
-		
-		for(var i = 0; i < btnNavSwitch.length; i++){
-			btnNavSwitch[i].classList.remove('btn-active');
-		};
-		target.classList.add('btn-active');
+if(btnNavContainer){
+
+	function removeActive(event){
+
+		var target = event.currentTarget;
+		var targetParent = target.parentNode.children;
+
+		for(var i = 0; i < targetParent.length; i++){
+			targetParent[i].classList.remove("btn-active");
+			target.classList.add("btn-active");
+		}
 	};
 
 	for(var i = 0; i < btnNavSwitch.length; i++){
-		btnNavSwitch[i].addEventListener("click", setActive, false);
+		btnNavSwitch[i].addEventListener("click", removeActive, false);
 	}
+
 };
